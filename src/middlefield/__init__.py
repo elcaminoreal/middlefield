5e5a13aed60e3a020ec090532bad186845f244c4
Moshe Zadka <zadka.moshe@gmail.com>
2017-12-18 19:57:54 -0800

    temp commit

---
+++ src/middlefield/__init__.py
@@ -0,0 +1,3 @@
+from middlefield._api import COMMANDS, run, self_build
+
+__all__ = ['COMMANDS', 'run', 'self_build']
---
+++ src/middlefield/__main__.py
@@ -0,0 +1,5 @@
+import sys
+
+import middlefield
+
+middlefield.run(sys.argv[1:], os.environ)

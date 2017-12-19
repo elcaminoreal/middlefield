417e740aa18ae75c8992810f0bd9b24d874a3c82
Moshe Zadka <zadka.moshe@gmail.com>
2017-12-18 19:43:41 -0800

    temp commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+# Copyright (c) Moshe Zadka
+# See LICENSE for details.
+from distutils import cmd, spawn
+
+import os
+import subprocess
+import sys
+
+import setuptools
+
+setuptools.setup(
+    packages=setuptools.find_packages(),
+    name='middlefield',
+    version='0.0.1',
+)

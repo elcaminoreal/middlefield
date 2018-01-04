b057ec50d0c2cb9c08156014ac594188ee6b2015
Moshe Zadka <zadka.moshe@gmail.com>
2018-01-03 18:36:15 -0800

    temp commit

--- src/middlefield/__init__.py
+++ src/middlefield/__init__.py
@@ -1,3 +1,6 @@
 from middlefield._api import COMMANDS, self_build
+from middlefield._version import __version__ as _raw_version
 
-__all__ = ['COMMANDS', 'self_build']
+__version__ = _raw_version.short()
+
+__all__ = ['COMMANDS', 'self_build', '__version__']
--- src/middlefield/_api.py
+++ src/middlefield/_api.py
@@ -1,13 +1,22 @@
-import elcaminoreal
-import caparg as cap
 import typing
 
+import caparg as cap
+import elcaminoreal
+import middlefield
+import seashore
+
 COMMANDS = elcaminoreal.Commands()
+
+@COMMANDS.dependency()
+def executor(_dependencies, _maybe_dependencies):
+    return seashore.Executor(seashore.Shell())
 
 @COMMANDS.command(
     parser=cap.command('',
         requirements=cap.option(type=typing.List[str], have_default=True),
         package=cap.option(type=typing.List[str], have_default=True),
-        output=cap.option(type=str, required=True)))
-def self_build(args, _dependencies):
-    print(args, _dependencies)
+        output=cap.option(type=str, required=True)),
+    dependencies=['executor'])
+def self_build(args, dependencies):
+    package = args['package'].append(f'middlefield={middlefield.__version__}')
+    print(package, args['requirements'], args['output'], dependencies)

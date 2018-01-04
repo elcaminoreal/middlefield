774f1e8c1d607f788cb70d16d7cca2b99f16cfb7
Moshe Zadka <zadka.moshe@gmail.com>
2018-01-03 18:23:07 -0800

    froop

--- src/middlefield/_api.py
+++ src/middlefield/_api.py
@@ -6,8 +6,8 @@ COMMANDS = elcaminoreal.Commands()
 
 @COMMANDS.command(
     parser=cap.command('',
-        requirements=cap.option(type=typing.List[str]),
-        package=cap.option(type=typing.List[str]),
+        requirements=cap.option(type=typing.List[str], have_default=True),
+        package=cap.option(type=typing.List[str], have_default=True),
         output=cap.option(type=str, required=True)))
 def self_build(args, _dependencies):
-    pass
+    print(args, _dependencies)
